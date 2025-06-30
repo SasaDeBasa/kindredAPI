43004cd99cb914da33bcb35757ecb91f84a707fb
SasaDeBasa <sasaekan1234@gmail.com>
2025-06-30 07:09:10 +0530

    Fix gunicorn

--- app.py
+++ app.py
@@ -3,12 +3,19 @@ import joblib
 
 app = Flask(__name__)
 
-# Load your trained model
-model = joblib.load("depression_model.pkl")
+# Load model
+model = joblib.load('depression_model.pkl')
 
-@app.route("/predict", methods=["POST"])
+@app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json(force=True)
-    features = data["features"]
+    data = request.get_json()
+    features = data['features']
     prediction = model.predict([features])
-    return jsonify({"prediction": prediction[0]})
+    return jsonify({'prediction': prediction[0]})
+
+@app.route('/')
+def home():
+    return "Mental Health Model is running!"
+
+if __name__ == '__main__':
+    app.run()
